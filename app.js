996b946cb0e91377664063e44b24ac0c4579f2bd
Casper Oakley <casper@casper-laptop.(none)>
2013-11-09 18:42:46 +0000

    test

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var express = require('express')
 , http = require('http')
 , app = express()
+, mongoose = require('mongoose')
 , color = require('colors')
 , routes = require('./routes');
-
 app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/mydb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("mongodb connected successfully".red)
+	var testSchema = mongoose.Schema({
+		name: String
+	})
+	var test = mongoose.model('Test1',testSchema)
+	var joe = new test({ name: 'yoyoyo' })
+});
 app.set('views', __dirname + '/views');
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', routes.index);
+app.get('/', function(req,res){
+	var iptest = req.connection.remoteAddress();
+	var gg = new test({ name: iptest});
+	console.log(gg.name);
+	routes.index;
+});
+
 app.get('/chocolates', routes.chocolates);
 
 var repeat = 4;
