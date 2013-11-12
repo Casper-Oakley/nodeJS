f2f84e139d7a31ff72479c3c73d64410ec7aa03b
Casper Oakley <casper@casper-laptop.(none)>
2013-11-12 00:32:04 +0000

    yo

--- public/js/socketTest.js
+++ public/js/socketTest.js
@@ -1,35 +1,34 @@
-window.onload = function() {
-
-	//var socket = io.connect('http://localhost:5000');
-	var socket = io.connect(window.location.hostname);
-	var field = document.getElementById("field");
-	var sendButton = document.getElementById("send");
-
 	var tag = document.createElement('script');
 	tag.src = "https://www.youtube.com/iframe_api";
 	var firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
 	var player;
 	function onYouTubeIframeAPIReady(){
-		console.log("ASIFUHADIUFHAEIDFB");
 		player = new YT.Player('player', {
 			height:'390',
 			width:'640',
-			videoID: 'M7lc1UVf-VE',
+			videoId: 'M7lc1UVf-VE',
 			events: {
 				'onReady': onPlayerReady,
 				'onStateChange': onPlayerStateChange
 			}
 		});
 	}
-	function stopVideo() {
-		player.stopVideo();
-	}
 	function onPlayerReady(event){
 		event.target.playVideo();
 	}
 function onPlayerStateChange(event) {
 	}
+	function stopVideo() {
+		player.stopVideo();
+	}
+
+window.onload = function() {
+
+	var socket = io.connect(window.location.hostname);
+	var field = document.getElementById("field");
+	var sendButton = document.getElementById("send");
+
 	socket.on('message', function (data) {
 		console.log(data);
 	});
